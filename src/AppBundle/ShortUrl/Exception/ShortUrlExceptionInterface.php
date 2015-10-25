7fd6ae40da424370296de25d95b13320d748324a
fzuellich <fzuellich@gmail.com>
2015-10-25 13:41:03 +0100

    Added exceptions

---
+++ src/AppBundle/ShortUrl/Exception/ShortUrlExceptionInterface.php
@@ -0,0 +1,6 @@
+<?php
+namespace AppBundle\ShortUrl\Exception;
+
+interface ShortUrlExceptionInterface {}
+
+?>
---
+++ src/AppBundle/ShortUrl/Exception/UrlNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace AppBundle\ShortUrl\Exception;
+
+class UrlNotFoundException extends \Exception implements ShortUrlExceptionInterface {}
+
+?>
