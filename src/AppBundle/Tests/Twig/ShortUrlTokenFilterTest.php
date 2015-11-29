e7a0d00adb9b87ca9560a3c7b07e507dcc8b885c
fzuellich <fzuellich@gmail.com>
2015-11-29 13:41:31 +0100

    Added unit test for twig filter.

---
+++ src/AppBundle/Tests/Twig/ShortUrlTokenFilterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Tests\Twig;
+
+use AppBundle\Twig\ShortUrlTokenFilter;
+
+class ShortUrlTokenFilterTest extends \PHPUnit_Framework_TestCase {
+
+	public function test_filter() {
+		$instance = new ShortUrlTokenFilter();
+
+		$expectation = array('dnh' => 12345, 'bkPtsG' => 1073741824, 'b5oA' => 458330);
+		foreach ($expectation as $key => $value) {
+			$this->assertEquals($key, $instance->shortUrlTokenFilter($value));
+		}
+
+
+		$this->assertEquals('', $instance->shortUrlTokenFilter(False));
+		$this->assertEquals('', $instance->shortUrlTokenFilter(Null));
+
+	}
+}
